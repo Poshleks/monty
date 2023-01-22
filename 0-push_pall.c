e5ab5a217db0a3bda2eb4f1d0d33c2f1e4d1f295
poshleks <adetunjahad@gmail.com>
2023-01-22 14:34:27 -0800

    task 0

--- 0-push_pall.c
+++ 0-push_pall.c
@@ -1,51 +1,273 @@
-o#include "monty.h"
-
-#include <ctype.h>
-
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <string.h>
-
-
+#include "monty.h"
 
 /**
  *
- *  * push_pall - pushes all values onto the stack
+ *  * process_file - function that read file line by line.
  *
- *   * @stack: double pointer to the head of the stack
+ *   * @file : file to read
  *
- *    * @line_number: line number of the opcode
+ *    * Return: nothing;
  *
  *     */
 
-void push_pall(stack_t **stack, unsigned int line_number) {
+void  process_file(FILE *file)
 
-	  stack_t *new;
+{
 
-	    (void)line_number;
+		char *line = NULL;
 
-	      new = malloc(sizeof(stack_t));
+			ssize_t read;
 
-	        if (new == NULL) {
+				size_t len = 0, j = 1;
 
-			    fprintf(stderr, "Error: malloc failed");
+					char **argl;
 
-			        exit(EXIT_FAILURE);
+						stack_t *stack = NULL;
 
-				  }
+							instruction_t operation_opcode;
 
-		  new->n = global;
 
-		    new->prev = NULL;
 
-		      new->next = *stack;
+								while ((read = getline(&line, &len, file)) != -1)
 
-		        if (*stack != NULL)
+										{
 
-				    (*stack)->prev = new;
+													if (validate_spaces(line))
 
-			  *stack = new;
+																{
+
+																				j++;
+
+																							continue;
+
+																									}
+
+															argl = token_line(line);
+
+
+
+																	if (!strcmp(argl[0], "push"))
+
+																					monty_push(&stack, argl[1], j);
+
+																			else
+
+																						{
+
+																										operation_opcode = search_opcode(argl[0]);
+
+																													if (operation_opcode.f != NULL)
+
+																																	{
+
+																																						operation_opcode.f(&stack, j);
+
+																																									}
+
+																																else
+
+																																				{
+
+																																									fprintf(stderr, "L%ld: unknown instruction %s\n", j, argl[0]);
+
+																																													exit(EXIT_FAILURE);
+
+																																																}
+
+																																		}
+
+																					free(argl);
+
+																							j++;
+
+																								}
+
+									free(line);
+
+										free_nodes(stack);
+
+}
+
+/**
+ *
+ *  * search_opcode - function that pushes an element to stack
+ *
+ *   * @opcode : look the opcode to run
+ *
+ *    * Return: a instruction_t structure
+ *
+ *     */
+
+instruction_t search_opcode(char *opcode)
+
+{
+
+		int i = 0;
+
+
+
+			instruction_t option[] = {
+
+						{"pall", monty_pall},
+
+								{"pint", monty_pint},
+
+										{"pop", monty_pop},
+
+												{"swap", monty_swap},
+
+														{"add", monty_add},
+
+																{"nop", monty_nop},
+
+																		{NULL, NULL}
+
+							};
+
+
+
+				for (i = 0; option[i].f != NULL; i++)
+
+						{
+
+									if (strcmp(option[i].opcode, opcode) == 0)
+
+												{
+
+																return (option[i]);
+
+																		}
+
+										}
+
+
+
+					return (option[i]);
+
+}
+
+/**
+ *
+ *  * token_line - function that split the line.
+ *
+ *   * @line : The pointer to bytecode line.
+ *
+ *    * Return: The pointer to tokens.
+ *
+ *     */
+
+char **token_line(char *line)
+
+{
+
+		char *token = NULL;
+
+			char **arg_tok = NULL;
+
+				int i;
+
+
+
+					arg_tok = malloc((get_num_words(line) + 2) * sizeof(char *));
+
+						if (!arg_tok)
+
+								{
+
+											fprintf(stderr, "Error: malloc failed");
+
+													exit(EXIT_FAILURE);
+
+														}
+
+							token = strtok(line, " ");
+
+								i = 0;
+
+									while (token)
+
+											{
+
+														arg_tok[i] = token;
+
+																token = strtok(NULL, " ");
+
+																		i++;
+
+																			}
+
+										arg_tok[i - 1][strlen(arg_tok[i - 1]) - 1] = '\0';
+
+											return (arg_tok);
+
+}
+
+/**
+ *
+ *  * get_num_words - function get number of words in a line.
+ *
+ *   * @line : string.
+ *
+ *    * Return: number of words.
+ *
+ *     */
+
+int get_num_words(char *line)
+
+{
+
+		int i = 0, n = 1;
+
+
+
+			while (line[i])
+
+					{
+
+								if (line[i] == ' ')
+
+												n++;
+
+										i++;
+
+											}
+
+				return (n);
+
+}
+
+/**
+ *
+ *  * validate_spaces - Validate spaces, tabs and line breaks
+ *
+ *   * @input: string from input
+ *
+ *    * Return: 1 if only find spaces, tabs or line break
+ *
+ *     */
+
+int validate_spaces(char *input)
+
+{
+
+		int i = 0, flag = 1, len = 0;
+
+
+
+			len = strlen(input);
+
+
+
+				while (input[i] == ' ' || input[i] == '\n' || input[i] == '\t')
+
+							i++;
+
+
+
+					if (i != len)
+
+								flag = 0;
+
+						return (flag);
 
 }
